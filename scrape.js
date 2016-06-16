badf04bfe6ab76c21cf17e6f899f0bd3dc63b1f1
Steve Schwartz <steve@alfajango.com>
2016-06-16 11:54:07 -0400

    Initial commit of working app

---
+++ scrape.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+var sendgrid = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY);
+var url = require('url');
+
+var getDeals = function(callback) {
+  var requestUrl = 'http://www.dealzon.com/home-theater/hdtvs?screen-size=60',
+      minSize = parseFloat(process.env.MIN_SIZE),
+      maxPrice = parseFloat(process.env.MAX_PRICE),
+      parsedUrl = url.parse(requestUrl),
+      domain = parsedUrl.protocol + "//" + parsedUrl.host;
+
+  request(requestUrl, function(error, response, html){
+    if(!error) {
+      var $ = cheerio.load(html);
+
+      var json = [];
+
+      $('ol.stream').find('li').each(function(){
+        var $li = $(this),
+            path = $li.find('h2').find('a').attr('href'),
+            link = domain + path,
+            text = $li.find('.byline_meta').text(),
+            size = parseFloat(text.match(/(.+)" Display/)[1]),
+            regularPrice = parseFloat($li.find('.strike_price').text().match(/\$(.+)/)[1].replace(',','')),
+            price = parseFloat($li.find('.price').text().match(/\$(.+)/)[1].replace(',','')),
+            location = $li.find('.section_2').text().match(/at (.+)/)[1],
+            expired = $li.hasClass('expired_styling');
+
+        if (size >= minSize && price <= maxPrice && !expired) {
+          json.push({"size": size, "regularPrice": regularPrice, "price": price, "location": location, "expired": expired, "link": link});
+        }
+      });
+
+      if (json.length) {
+        var helper = require('sendgrid').mail,
+            from_email = new helper.Email(process.env.EMAIL_FROM),
+            to_email = new helper.Email(process.env.EMAIL_TO),
+            subject = "New Steve & Kevin TV Deal Alert",
+            content = news helper.Content("text/plain", json.map(function() { return "Size: " + this.size + ", Price: " + this.price + ", Link: " + this.link; }).join('\n\n')),
+            mail = new helper.Mail(from_email, subject, to_email, content),
+            requestBody = mail.toJSON(),
+            request = sg.emptyRequest();
+
+        request.method = 'POST'
+        request.path = '/v3/mail/send'
+        request.body = requestBody
+
+        sg.API(request, function (response) {
+          console.log(response.statusCode)
+          console.log(response.body)
+          console.log(response.headers)
+        })
+      }
+
+      console.log(json);
+
+      callback(html);
+    }
+  });
+};
+
+app.get('/scrape', function(req, res){
+
+  getDeals(function(html) {
+    res.send(html)
+  });
+
+})
+
+var port = process.env.PORT || 3000;
+app.listen(port)
+console.log("Visit http://localhost:" + port);
+exports = module.exports = app;
